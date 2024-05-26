2b2d2cc3cf6226fd4dce828778df3803ae8a6414
Marien Zwart <marien.zwart@gmail.com>
2024-05-26 14:20:32 +1000

    Add fetch override for ol-notmuch
    Noticed through upcoming test coverage improvement.

--- fetch-overrides.nix
+++ fetch-overrides.nix
@@ -29,6 +29,8 @@
     undo-fu = "https://codeberg.org/ideasman42/emacs-undo-fu.git";
     undo-fu-session = "https://codeberg.org/ideasman42/emacs-undo-fu-session.git";
     consult-notmuch = "https://codeberg.org/jao/consult-notmuch.git";
+    # sourcehut fetcher doesn't work for the same reason codeberg doesn't.
+    ol-notmuch = "https://git.sr.ht/~tarsius/ol-notmuch";
     # nixpkgs uses a release from nongnu ELPA.
     corfu-terminal = "https://codeberg.org/akib/emacs-corfu-terminal";
   };
