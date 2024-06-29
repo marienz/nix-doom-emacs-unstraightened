5f7f3b93868805e0a9be600807a2b3db2ef22c19
Marien Zwart <marien.zwart@gmail.com>
2024-06-29 20:57:01 +1000

    Just build module flag combinations, omit run test

--- checks.nix
+++ checks.nix
@@ -62,6 +62,7 @@ let
       tmux kill-session -t doom-testing
       '';
   };
+  doomBuildTest = init: mkDoom { doomDir = mkDoomDir init; };
 in {
   minimal = mkDoom { doomDir = minimalDoomDir; };
   minimalEmacs = (makeDoomPackages (common // {
@@ -91,23 +92,23 @@ in {
   unpinned-org = doomTest "external-org" { app.rss = [ "+org" ]; } { };
   # Dependencies that require a module flag enabled and a different module or flag disabled.
   # flycheck-eglot needs flymake disabled.
-  flycheck-eglot = doomTest "nix-profile" { tools.lsp = [ "+eglot" ]; checkers.syntax = true; } { };
+  flycheck-eglot = doomBuildTest { tools.lsp = [ "+eglot" ]; checkers.syntax = true; };
   # lsp-python-ms needs +pyright disabled.
-  lsp-python-ms = doomTest "nix-profile" { lang.python = [ "+lsp" ]; tools.lsp = true; } { };
+  lsp-python-ms = doomBuildTest { lang.python = [ "+lsp" ]; tools.lsp = true; };
   # roam2 needs +roam disabled.
-  org-roam2 = doomTest "nix-profile" { lang.org = [ "+roam2" ]; } { };
+  org-roam2 = doomBuildTest { lang.org = [ "+roam2" ]; };
   # nerd-icons-dired needs +dirvish disabled.
-  nerd-icons-dired = doomTest "nix-profile" { emacs.dired = [ "+icons" ]; } { };
+  nerd-icons-dired = doomBuildTest { emacs.dired = [ "+icons" ]; };
   # multiple-cursors needs :editor evil disabled.
-  multiple-cursors = doomTest "nix-profile" { editor.multiple-cursors = true; } { };
+  multiple-cursors = doomBuildTest { editor.multiple-cursors = true; };
   # flx needs +prescient disabled.
-  flx = doomTest "nix-profile" { completion.ivy = [ "+fuzzy" ]; } { };
+  flx = doomBuildTest { completion.ivy = [ "+fuzzy" ]; };
   # corfu pulls in unpinned orderless if vertico is disabled.
-  corfu-orderless = doomTest "nix-profile" { completion.corfu = [ "+orderless" ]; } { };
+  corfu-orderless = doomBuildTest { completion.corfu = [ "+orderless" ]; };
   # flyspell can pull in one of three completion modules.
-  flyspell-correct-ivy = doomTest "nix-profile" { checkers.spell = [ "+flyspell" ]; completion.ivy = true; } { };
-  flyspell-correct-helm = doomTest "nix-profile" { checkers.spell = [ "+flyspell" ]; completion.helm = true; } { };
-  flyspell-correct-popup = doomTest "nix-profile" { checkers.spell = [ "+flyspell" ]; } { };
+  flyspell-correct-ivy = doomBuildTest { checkers.spell = [ "+flyspell" ]; completion.ivy = true; };
+  flyspell-correct-helm = doomBuildTest { checkers.spell = [ "+flyspell" ]; completion.helm = true; };
+  flyspell-correct-popup = doomBuildTest { checkers.spell = [ "+flyspell" ]; };
 
   extraPackages = doomTest "extraPackages" { config.default = true; } { extraPackages = epkgs: [ epkgs.vterm ]; };
 }
