8626f4684eefc1b3976a39092b4d69c653bfa7ac
Marien Zwart <marien.zwart@gmail.com>
2024-11-03 00:00:43 +1100

    Pin forge
    Relative to the version of Doom we currently pull in, this is an upgrade
    from 0.4.3 to 0.4.4 (no significant changes).
    
    Relative to the version Doom currently pulls in, this is a downgrade of
    one commit, because it looks like that commit depends on a snapshot of
    transient newer than what emacs-overlay currently provides.
    
    https://github.com/marienz/nix-doom-emacs-unstraightened/issues/37
    tracks removing this pin again.

--- fetch-overrides.nix
+++ fetch-overrides.nix
@@ -45,5 +45,7 @@
     sly-stepper = "da84e3bba8466c2290c2dc7c27d7f4c48c27b39e";
     # In emacsattic, so shouldn't change underneath us.
     ammonite-term-repl = "b552fe21977e005c1c460bf6607557e67241a6b6";
+    # TODO: drop this pin again, see https://github.com/marienz/nix-doom-emacs-unstraightened/issues/37
+    forge = "c819271dd056d9608e6dd7e788bb3007f0a44517";
   };
 }
