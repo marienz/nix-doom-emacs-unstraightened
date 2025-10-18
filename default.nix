ffa20f8e4df26db4bfb8e294c5db6fc707d86a8f
Marien Zwart <marien.zwart@gmail.com>
2025-10-18 12:28:43 +1100

    Handle preBuild being null
    A handful of packages in nixpkgs set preBuild conditionally (based on
    version) but set it to null when they intend to leave it unchanged (and
    the original they patch do not have the attribute). That breaks our
    further patching.
    
    Work around this by checking for null.
    
    I don't do this more broadly (for the overrides in
    elisp-packages-late.nix) because I do not think we should need to be
    this careful. We can add more workarounds if we find module combinations
    that actually trigger the problem there.

--- default.nix
+++ default.nix
@@ -403,7 +403,12 @@ let
           in
           pkg.overrideAttrs (
             lib.optionalAttrs isLspModeOrDependant {
-              preBuild = (pkg.preBuild or "") + ''
+              # TODO: simplify if https://github.com/NixOS/nixpkgs/pull/452898 is merged.
+              preBuild = let
+                origPreBuild = pkg.preBuild or "";
+                origPreBuildString = if origPreBuild == null then "" else origPreBuild;
+              in
+                origPreBuildString + ''
                 export LSP_USE_PLISTS=1
               '';
             }
