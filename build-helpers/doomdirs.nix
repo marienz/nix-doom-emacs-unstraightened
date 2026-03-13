9f275c681e22a663483a1e9018e7b55749f618a5
Marien Zwart <marien.zwart@gmail.com>
2026-03-13 21:38:40 +1100

    CI: more concise Ada support check

--- build-helpers/doomdirs.nix
+++ build-helpers/doomdirs.nix
@@ -23,7 +23,7 @@
 let
   # https://github.com/NixOS/nixpkgs/blob/f4501c9681b56c207927782b4c76a07190a58ab9/pkgs/development/compilers/gnat-bootstrap/default.nix
   # We might be able to drop this once nixpkgs defaults to gnat 14.
-  adaUnsupported = stdenv.hostPlatform.system == "aarch64-darwin";
+  adaUnsupported = !stdenv.hostPlatform.isx86;
   commonArgs = "-o $out" + (lib.optionalString adaUnsupported " -s '(:lang ada)'");
   allModules = callPackage ./doomscript.nix {
     name = "doom-full-init";
