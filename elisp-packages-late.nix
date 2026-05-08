b834753afca6b8caaaa534bdc7fabb85345ed624
Marien Zwart <marien.zwart@gmail.com>
2026-05-08 21:55:33 +1000

    Apply upstream PR for evil-numbers issue
    evil-numbers uses incf before it is available. Doom fixed this by
    aliasing incf to cl-incf, but had to disable byte compilation for
    evil-numbers for that fix to work.
    
    Instead of disabling byte compilation, we can relatively easily apply
    the upstream fix.

--- elisp-packages-late.nix
+++ elisp-packages-late.nix
@@ -17,6 +17,7 @@
 {
   eself,
   esuper,
+  fetchpatch2,
   git,
   lib,
   makeWrapper,
@@ -253,6 +254,18 @@ let
       # ];
       ignoreCompilationError = true;
     };
+
+    evil-numbers = esuper.evil-numbers.overrideAttrs (old: {
+      # Apply pending upstream fix as an alternative to
+      # https://github.com/doomemacs/doomemacs/commit/541207196fd1ec71bc81393f57ca91263c23b682
+      # (which does not work when evil-numbers is byte-compiled)
+      patches = (old.patches or []) ++ [
+        (fetchpatch2 {
+          url = "https://github.com/juliapath/evil-numbers/pull/31/commits/2229ad6df398f4c959981b543c467ed6b89320a2.patch";
+          hash = "sha256-PAklkvKyiZ04QikbmtLD1tlfcfjeNDNpbDol9OcITGU=";
+        })
+      ];
+    });
 
     # Other files that fail to byte-compile:
     # - rustic-flycheck, no flycheck dependency. Seems undesirable to force.
