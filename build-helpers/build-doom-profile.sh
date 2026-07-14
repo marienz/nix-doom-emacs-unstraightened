c89e503b036611f311d7e23b39c8ac0d9a65daff
Marien Zwart <marien.zwart@gmail.com>
2026-07-15 00:08:32 +1000

    Drop non-package.el autoload generation
    I originally added this to generate autoloads for trivialBuild packages:
    commit fa6fe4cf937e25dfcf1a44bd4d69ecbca280a85f used cmake to test it
    (which was a trivialBuild (in our own elisp-packages.nix) at that time)
    and used the "copilot" package as motivation (which was a trivialBuild
    in nixpkgs).
    
    But since then, I removed all uses of trivialBuild from this project,
    and nixpkgs has also retired trivialBuild (see
    https://github.com/NixOS/nixpkgs/issues/278925)... exactly because these
    packages do not activate at startup and a normal user would need to
    manually load their autoloads (same problem I was working around by
    adding those autoloads to our profile).
    
    That tracking issue says a few packages remain, but one has been removed
    since and the other provides only a module: that leaves this code dead
    unless I reintroduce a trivialBuild just to test it with.
    
    That does not seem worth it: as far as I can tell users cannot hit this
    code with nixpkgs+emacs-overlay anymore (they would need an overlay of
    their own to reintroduce a non-package.el package).
    
    Drop the dead code (which I further confirmed is actually dead by
    running `nix flake check` with `generate-nix-autoloads` modified to
    error out if an autoload is found: this confirmed no package covered by
    our tests has an autoload file this code picked up).

--- build-helpers/build-doom-profile.sh
+++ build-helpers/build-doom-profile.sh
@@ -32,9 +32,7 @@ $runtimeShell $doomSource/bin/doomscript $buildProfileLoader \
 export HOME="$PWD/home"
 export DOOMPROFILE="$profileName";
 export DOOMPROFILELOADFILE=$out/loader/init
-$runtimeShell $doomSource/bin/doomscript $buildProfile \
-    -i $initEl \
-    -l $deps/share/emacs/site-lisp
+$runtimeShell $doomSource/bin/doomscript $buildProfile -i $initEl
 
 # Similar to audit-tmpdir.sh in nixpkgs.
 if grep -q -F "$TMPDIR/" -r $out; then
