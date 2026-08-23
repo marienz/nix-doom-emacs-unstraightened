b813c27d4025e964ad9c8801ba8a8d9d2fe5f836
Marien Zwart <marien.zwart@gmail.com>
2026-08-23 21:51:55 +1000

    Pin swift-mode to latest version
    This is slightly past the version Doom is pinned to, which seems to not
    work on Emacs 31 on MacOS.

--- fetch-overrides.nix
+++ fetch-overrides.nix
@@ -54,8 +54,8 @@
     # evil-ghostel is tightly coupled to ghostel: unpin it too.
     evil-ghostel = null;
 
-    # Newer version seems to trigger breakage in Emacs 31
-    swift-mode = "cfae3b85ad09bd293df941261afbc21e41bbb5f8";
+    # Try latest version to see if it works on Emacs 31 again.
+    swift-mode = "053336346073e6e2849c19cadd6c6e3e79985c2d";
   };
 
   # :files passed in to melpa2nix (currently only if not already present in recipe).
