marienz/nix-doom-emacs-unstraightened

commit 562d95f9bd8a9fbffdf81ae51a2ca547192d5bd2

Improve dependency extraction
It turns out package-dir-info has some problems: the way it locates
-pkg.el files meant it was not actually doing so for us (it relied on
the directory name matching the package name), and it then falls back to
the first plausible-looking .el files: that may not be the right one if
we build multiple packages out of the same repository checkout.

This causes a build failure after the next emacs-overlay update, which
drops company-lean and lean-mode from Melpa (meaning our dependency
extraction needs to work on those).

Try to fix it by strictly extracting dependency info from the NAME.el
file.

Make dependency extraction failure fail the build. We run it on few
enough packages I think this is worth it (to catch bugs).

This has two annoyances:

- It uses some package.el internals that moved in Emacs 30, so we need
  two codepaths.
- We have one package (x-face-e21) for which the NAME.el file is not in
  the root. For now, add a hack that looks in one level of subdir.
  The proper fix would be to use :files from the recipe...

This is not sufficient to make lean-mode build, but should be closer.

build-helpers/print-deps.el
@@ -13,16 +13,45 @@
 ;; limitations under the License.
 
 (require 'package)
+(require 'lisp-mnt)
 
-(with-temp-buffer
-  (setq default-directory (car command-line-args-left))
-  (dired-mode)
-  ;; Ignore dependency extraction errors because it fails for repos not
-  ;; containing a "proper" package (no -pkg.el, no file with the right magic
-  ;; header). These seem common enough to be not worth allowlisting.
-  (let ((reqs (with-demoted-errors "Extracting dependencies: %s"
-                (package-desc-reqs (package-dir-info)))))
-    (princ
-     (json-encode
-      (mapcar #'car (seq-remove (lambda (p) (apply #'package-built-in-p p))
-                                reqs))))))
+;; package-dir-info is awkward:
+;; - When looking for -pkg.el, it extracts the package name from the directory.
+;;   That does not work for us: our directory is a store path.
+;; - When looking for .el files with package headers, it accepts the first one
+;;   for which package-buffer-info returns non-nil.
+;;   That does not have the same problem, but it has a different one:
+;;   things like company-lean and lean-mode build from the same repo.
+;;
+;; So we pass the package name in explicitly and roll our own.
+;;
+;; Melpa is trying to phase out -pkg.el files. Since we need this for fewer
+;; packages than Melpa does, try to do the same.
+;;
+;; And package-buffer-info requires a Version or Package-Version header not
+;; everything has, so grab dependencies directly.
+
+(let* ((directory (car command-line-args-left))
+       (name (cadr command-line-args-left))
+       (file (expand-file-name (format "%s.el" name) directory))
+       (file (if (file-exists-p file)
+                 file
+               ;; HACK for x-face-e21.el (normally found through recipe)
+               (car (file-expand-wildcards
+                     (format "%s/*/%s.el" directory name)))))
+       (reqs (if (fboundp 'lm-package-requires)
+                 ;; Emacs >= 30
+                 (lm-package-requires file)
+               ;; Emacs 29
+               (lm-with-file file
+                 (and-let* ((require-lines (lm-header-multiline
+                                            "package-requires")))
+                   (package--prepare-dependencies
+                    (package-read-from-string
+                     (string-join require-lines " ")))))))
+       (desc (package-desc-from-define name "9999snapshot" nil reqs))
+       (parsed-reqs (package-desc-reqs desc))
+       (filtered-reqs (seq-remove (lambda (p) (apply #'package-built-in-p p))
+                                  parsed-reqs))
+       (req-names (mapcar #'car parsed-reqs)))
+  (princ (json-encode req-names)))

default.nix
@@ -344,10 +344,10 @@ let
               builtins.fetchGit fetchGitArgs;
           # Run locally to avoid a network roundtrip.
           reqfile = runCommandLocal "${name}-deps" {
-            inherit src;
+            inherit src name;
             emacs = lib.getExe emacs;
             printDeps = ./build-helpers/print-deps.el;
-          } "$emacs -Q --batch --script $printDeps $src > $out";
+          } "$emacs -Q --batch --script $printDeps $src $name > $out";
           reqjson = lib.importJSON reqfile;
           # json-encode encodes the empty list as null (nil), not [].
           reqlist = if reqjson == null then [ ] else reqjson;

commit 7327d29f04c9bf2bd133ae3d0a3ed0af283bced8

Ignore built-ins in automated dependency detection
This fixes a mistake introduced back in commit
562d95f9bd8a9fbffdf81ae51a2ca547192d5bd2.

This _might_ break something, because it can result in a package being
byte-compiled against the version of a dependency bundled with Emacs
while at runtime a newer version is pulled in (by some other package
requiring a newer version or getting its dependencies from nixpkgs /
emacs-overlay / MELPA, which do not apply the same filtering).

But the version skew there is only problematic if the dependency has
changes that affect byte compilation, which I think is rare enough this
should work.

build-helpers/print-deps.el
@@ -43,5 +43,5 @@
        (desc (package-desc-from-define name "9999snapshot1" nil reqs))
        (parsed-reqs (package-desc-reqs desc))
        (filtered-reqs (cl-remove #'package-built-in-p parsed-reqs :test #'apply))
-       (req-names (mapcar #'car parsed-reqs)))
+       (req-names (mapcar #'car filtered-reqs)))
   (princ (json-encode req-names)))